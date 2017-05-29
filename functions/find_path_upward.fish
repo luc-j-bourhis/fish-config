6a2f5573018149399b7e588749f43bf57641e02b
Luc J. Bourhis <luc_j_bourhis@mac.com>
2017-05-29 11:24:13 +0200

    New function searching directory hierarchy upward for an existing path

---
+++ functions/find_path_upward.fish
@@ -0,0 +1,26 @@
+function find_path_upward \
+    --description 'Walk directory hierarchy upward to find a file or a directory' \
+    -a basename
+
+    # Look in current directory
+    if test -f $basename -o -d $basename
+        echo $basename
+        return
+    end
+
+    # Walk upward
+    set -l dir '..'
+    while test (realpath $dir) != '/'
+        set -l path "$dir/$basename"
+        if test -f $path -o -d $path
+            echo $path
+            return
+        end
+        set -l dir "../$dir"
+    end
+    if test -f "/$basename" -o -d "/$basename"
+        echo "/$basename"
+    else
+        echo ''
+    end
+end

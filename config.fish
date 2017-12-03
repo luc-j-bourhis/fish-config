403b3b6831748304e72fa627abd25d0915743b3e
Luc J. Bourhis <luc_j_bourhis@mac.com>
2017-12-03 14:21:47 +0100

    Support for Perl local::lib

--- config.fish
+++ config.fish
@@ -10,6 +10,11 @@ if test -d ~/.plenv
     plenv init - fish | source
 end
 
+# local::lib
+if test -d ~/.perl5
+    eval (perl -I$HOME/.perl5/lib/perl5 -Mlocal::lib=$HOME/.perl5)
+end
+
 # pyenv
 if test -d ~/.pyenv
     prepend_to_fish_user_paths ~/.pyenv/bin $PATH

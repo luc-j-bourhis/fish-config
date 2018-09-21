1797593bf896d8b43cc5690c14038dbe1c836fa5
Luc J. Bourhis <luc_j_bourhis@mac.com>
2018-09-21 23:12:42 +0200

    Graciously fail if not on MacOS or the Applescript is not there

--- functions/open_url_with_safari.fish
+++ functions/open_url_with_safari.fish
@@ -1,4 +1,8 @@
-function open_url -a loc -d 'Open the given location in the default browser'
+function open_url_with_safari -a loc -d 'Open the given location in Safari browser'
+    if test (uname) != Darwin
+        echo (status function) is only available on MacOS
+        return
+    end
     # If `fish_help_browser` is set to this function, then `help` will call
     # `open_url --version` because `open_url` is not listed as a graphical browser
     # and fish then tests whether the browser is Lynx. Hence this hack: we ignore
@@ -6,5 +10,12 @@ function open_url -a loc -d 'Open the given location in the default browser'
     if test $loc = --version
         return
     end
-    osascript -e "tell (load script \"$my_applescript_folder/Open Location.scpt\") to openLocation(\"$loc\")"
+    set applescript "Open Location.scpt"
+    set applescript_path (dirname (status current-filename))/$applescript
+    if test ! -f $applescript_path
+        echo (status function) cannot work without the Applescript \"$applescript\"
+        echo Available from my Applescript github repo
+        return 1
+    end
+    osascript -e "tell (load script \"$applescript_path\") to openLocation(\"$loc\")"
 end

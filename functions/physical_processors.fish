760581e5b7d665f3365521410cf1ce87dda546ef
Luc J. Bourhis <luc_j_bourhis@mac.com>
2019-07-24 22:10:46 +0900

    Remove function make obsolete by cpu_info

--- functions/physical_processors.fish
+++
@@ -1,9 +0,0 @@
-function physical_processors -d "Number of physical processors"
-    switch (uname -o)
-        case Cygwin    
-            grep 'physical id' /proc/cpuinfo|sort --unique|wc --lines
-        case '*'
-            echo unknown
-    end
-end
-

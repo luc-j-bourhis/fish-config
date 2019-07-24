9d9d8b5f699d386da5d48aa37ab02104f55c9d39
Luc J. Bourhis <luc_j_bourhis@mac.com>
2019-07-24 22:51:24 +0900

    Only long options, add --cpus

--- functions/cpu_info.fish
+++ functions/cpu_info.fish
@@ -1,20 +1,29 @@
-function cpu_info --no-scope-shadowing
-    argparse -n cpu_info -x p,l -X 0 'p/physical' 'l/logical' -- $argv
+function cpu_info --no-scope-shadowing \
+    -d "The computer has one or more CPU's, each with one or more cores, each running one or more thread"
+    argparse -n cpu_info -x p,l,c -X 0 \
+        'p-cores' \
+        'l-threads' \
+        'c-cpus' \
+        -- $argv
     or return
     set -l key
     switch (uname)
     case Linux 'CYGWIN*'
-        if set -q _flag_p
+        if set -q _flag_cores
             set key 'core id'
-        else if set -q _flag_l
+        else if set -q _flag_threads
             set key processor
+        else if set -q _flag_cpus
+            set key 'physical id'
         end
         count (grep $key /proc/cpuinfo|sort --unique)
     case Darwin
-        if set -q _flag_p
+        if set -q _flag_cores
             set key machdep.cpu.core_count
-        else if set -q _flag_l
+        else if set -q _flag_threads
             set key machdep.cpu.thread_count
+        else if set -q _flag_cpus
+            set key hw.packages
         end
         sysctl -n $key
     case '*'
@@ -22,5 +31,6 @@ function cpu_info --no-scope-shadowing
     end
 end
 
-complete -c cpu_info -s p -l physical -d 'Number of physical cores'
-complete -c cpu_info -s l -l logical -d 'Number of logical cores'
+complete -c cpu_info -l cores -d 'Number of cores'
+complete -c cpu_info -l threads -d 'Number of threads'
+complete -c cpu_info -l cpus -d 'Number of cpus'

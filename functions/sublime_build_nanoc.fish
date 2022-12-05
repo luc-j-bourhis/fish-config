b2c500b8f251277352dd57449bf994a71ed7a174
Luc J. Bourhis <luc_j_bourhis@mac.com>
2022-12-05 13:10:52 +0100

    Complete overhaul of Nanoc build script for Sublime Text

--- functions/sublime_build_nanoc.fish
+++ functions/sublime_build_nanoc.fish
@@ -1,13 +1,25 @@
-function sublime_build_nanoc --a filepath -d 'Sublime Text build scheme for nanoc site, which opens the page currently worked on in Safari (MacOS only)'
-	bundle exec nanoc
-    or exit $status
-    if string match -q '*.md' $filepath
-        set -U last_nanoc_url (string replace -r '^.*/content/' 'http://192.168.1.21:4000/' (dirname $filepath))
+function sublime_build_nanoc -a root filepath -d 'Sublime Text build scheme for nanoc site, which opens the page currently worked on in Safari (MacOS only)'
+  pwd
+	/usr/local/bin/rbenv exec bundle exec nanoc
+    or return $status
+  set page (string replace -r '^(.*/)?(test_?)content/' '' $filepath)
+  and begin
+    set name (basename $page)
+    set page (dirname $page)
+    if string match -q -r '\.(md|adoc|html)$' $name
+      if string match -q -r test_content $filepath
+        set page "tests/$page"
+      end
+      set url "$root/$page"
+    else
+      set url "$root/"
     end
-    echo Opening $last_nanoc_url ...
-    if test (osascript \
-      -e 'set m to load script file ((path to home folder as text) & "Developer:AppleScript:Open Safari Tab by URL.scpt")' \
-      -e "tell m to showTab given someName:\"$last_nanoc_url\"") = false
-        open $last_nanoc_url
-    end
+    set -U last_nanoc_url $url
+  end
+  echo Opening $last_nanoc_url ...
+  if test (osascript \
+    -e 'set m to load script file ((path to home folder as text) & "Developer:AppleScript:Open Safari Tab by URL.scpt")' \
+    -e "tell m to showTab given someName:\"$last_nanoc_url\"") = false
+      open $last_nanoc_url
+  end
 end

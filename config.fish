c6f74c36e2431311ada710a715b6c0c954cb14b0
Luc J Bourhis <luc_j_bourhis@mac.com>
2023-08-03 08:38:42 +0200

    Fix a problem on Linux with ls

--- config.fish
+++ config.fish
@@ -62,3 +62,6 @@ if string match -q 'CYGWIN*' $kernel_name
     set -U kernel_name Cygwin
 end
 
+# Disable single-quotes around path with spaces in the output of command ls
+# A really stupid change introduced in coreutils in recent years
+set -x QUOTING_STYLE literal

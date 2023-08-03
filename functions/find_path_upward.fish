9124a06443b7cbea375315fc586b7e957973c8d9
Luc J Bourhis <luc_j_bourhis@mac.com>
2023-08-03 08:43:12 +0200

    Rewrite find_path_upward

--- functions/find_path_upward.fish
+++ functions/find_path_upward.fish
@@ -2,21 +2,15 @@ function find_path_upward \
     --description 'Walk directory hierarchy upward to find a file or a directory' \
     -a basename
 
-    # Look in current directory
-    if test -f $basename -o -d $basename
-        echo $basename
-        return
-    end
-
     # Walk upward
-    set -l dir '..'
-    while test (realpath $dir) != '/'
-        set -l path "$dir/$basename"
+    set -l dir $PWD
+    while test $dir != '/'
+        set path "$dir/$basename"
         if test -f $path -o -d $path
             echo $path
             return
         end
-        set -l dir "../$dir"
+        set dir (dirname $dir)
     end
     if test -f "/$basename" -o -d "/$basename"
         echo "/$basename"

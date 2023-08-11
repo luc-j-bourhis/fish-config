ff115a68ef4e49c9c08bc2649be66c7ca5028160
Luc J Bourhis <luc_j_bourhis@mac.com>
2023-08-11 11:26:47 +0200

    Make checkports more robust

--- functions/checkports.fish
+++ functions/checkports.fish
@@ -1,6 +1,6 @@
 function checkports -d 'checkports IP port [port] [port] ...: run nmap on each port and print a clean report'
     for p in $argv[2..]
-        nmap $argv[1] -p $p
+        nmap $argv[1] -Pn -p $p
     end \
     | perl -lne '/^PORT/ and do {print unless  $header++}; m{^\d+/} and print'
 end

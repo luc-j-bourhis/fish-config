228ea051c756065f75211f1932569928bae406a2
Luc J Bourhis <luc_j_bourhis@mac.com>
2023-08-14 18:13:58 +0200

    Convenient wrapper around Microsoft azcopy utilisy for Azure Storage

---
+++ functions/az.fish
@@ -0,0 +1,30 @@
+function az -d 'Wrapper around azcopy for our Limmat container'
+    if not set -q AZURE_HOST
+        echo 'Please define environment variable AZURE_HOST'
+        return 1
+    end
+    if not set -q AZURE_SAS
+        echo 'Please define environment variable AZURE_SAS: its value is the shared access secret chosen in Azure storage manager'
+        return 1
+    end
+    argparse 'h/help' -- $argv
+    if test -n "$_flag_help"
+        echo "- az dirs: list the top directories of the container"
+        echo "- az CMD args..: pass command and arguments to azcopy"
+        echo "In all cases, a prefix 'az:' stands for the Azure storage host"
+        return
+    end
+    for i in (seq (count $argv))
+        set a $argv[$i]
+        if ! string match -qr '^--' -- $a; and test (string sub -l 3 $a) = 'az:'
+            set argv[$i] $HOST(string sub -s 4 $a)$SAS
+        end
+    end
+    switch $argv[1]
+        case dirs
+            azcopy list $argv[2..] | perl -pe '$_ = "" if /^$|newer version/; s{^INFO:\s*([^/]+).*$}{$1};' | uniq
+        case '*'
+            azcopy $argv
+    end
+end
+

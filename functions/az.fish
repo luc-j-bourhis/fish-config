022d05c6c46503c47db7a2ec2907c82310af74ff
Luc J Bourhis <luc.bourhis@bruker.com>
2023-08-16 12:04:14 +0000

    Support -r shortcut for azcopy wrapper

--- functions/az.fish
+++ functions/az.fish
@@ -7,7 +7,7 @@ function az -d 'Wrapper around azcopy for our Limmat container'
         echo 'Please define environment variable AZURE_SAS: its value is the shared access secret chosen in Azure storage manager'
         return 1
     end
-    argparse 'h/help' -- $argv
+    argparse -i 'h/help' 'r/recursive' -- $argv
     if test -n "$_flag_help"
         echo "- az dirs: list the top directories of the container"
         echo "- az CMD args..: pass command and arguments to azcopy"
@@ -16,10 +16,15 @@ function az -d 'Wrapper around azcopy for our Limmat container'
     end
     for i in (seq (count $argv))
         set a $argv[$i]
-        if ! string match -qr '^--' -- $a; and test (string sub -l 3 $a) = 'az:'
-            set argv[$i] $AZURE_HOST(string sub -s 4 $a)$AZURE_SAS
+        if not string match -qr '^--' -- $a; and test (string sub -l 3 $a) = 'az:'
+            set edited $AZURE_HOST(string sub -s 4 $a)$AZURE_SAS
+            set argv[$i] $edited 
         end
     end
+    if test -n "$_flag_recursive"
+        set argv $argv --recursive=true
+    end
+    echo $argv
     switch $argv[1]
         case dirs
             azcopy list $argv[2..] | perl -pe '$_ = "" if /^$|newer version/; s{^INFO:\s*([^/]+).*$}{$1};' | uniq

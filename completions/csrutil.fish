a9168bd958f678f0bccaab55c6d9f27e7451a3b0
Luc J. Bourhis <luc_j_bourhis@mac.com>
2024-02-22 22:42:54 +0100

    Completion for csrutil

---
+++ completions/csrutil.fish
@@ -0,0 +1,6 @@
+complete -c csrutil -f
+complete -c csrutil -a clear   -d 'Clear the existing configuration'
+complete -c csrutil -a disable -d 'Disable the protection on the machine. Only available in Recovery OS'
+complete -c csrutil -a enable  -d 'Enable the protection on the machine. Only available in Recovery OS'
+complete -c csrutil -a status  -d 'Display the current configuration'
+complete -c csrutil -a netboot

077a70e3d1160a736d518f78474be91ca39ce72e
Luc J Bourhis <luc_j_bourhis@mac.com>
2025-08-19 17:27:41 +0900

    Add a --all option to cpu_info
    Refactor code to do so

--- functions/cpu_info.fish
+++ functions/cpu_info.fish
@@ -1,36 +1,49 @@
-function cpu_info --no-scope-shadowing \
-    -d "The computer has one or more CPU's, each with one or more cores, each running one or more thread"
-    argparse -n cpu_info -x p,l,c -X 0 \
-        'p-cores' \
-        'l-threads' \
-        'c-cpus' \
+function cpu_info --no-scope-shadowing -d "Number of available CPUs, cores, or threads"
+    argparse -x h,p,l,c -X 0 \
+        'h/help' \
+        'p/cores' \
+        'l/threads' \
+        'c/cpus' \
+        'all' \
         -- $argv
-    or return
-    set -l key
+    or return 1
+    if set -q _flag_help
+        echo 'Usage: cpu_info [-h | --help] [-p | --cores] [-l | --threads] [-c | --cpus] [--all]'
+        echo '       (options are all exclusive)'
+        return
+    end
+    set -l info
     switch (uname)
     case Linux
-        if set -q _flag_cores
-            set key 'core id'
-        else if set -q _flag_threads
-            set key processor
-        else if set -q _flag_cpus
-            set key 'physical id'
+        for key in 'physical id' 'core id' 'processor'
+            set -a info (count (grep $key /proc/cpuinfo | sort --unique))
         end
-        count (grep $key /proc/cpuinfo|sort --unique)
     case Darwin
-        if set -q _flag_cores
-            set key machdep.cpu.core_count
-        else if set -q _flag_threads
-            set key machdep.cpu.thread_count
-        else if set -q _flag_cpus
-            set key hw.packages
+        for key in hw.packages machdep.cpu.core_count machdep.cpu.thread_count
+            set -a info (sysctl -n $key)
         end
-        sysctl -n $key
     case '*'
-        echo 'Unknown'
+        echo 'Error: unsupported platform'
+        return 1
+    end
+    set cpus $info[1]
+    set cores $info[2]
+    set threads $info[3]
+    if set -q _flag_cores
+        echo $cores
+    else if set -q _flag_threads
+        echo $threads
+    else if set -q _flag_cpus
+        echo $cpus
+    else if set -q _flag_all
+        echo "$cpus cpus"
+        echo "$cores cores"
+        echo "$threads threads"
     end
 end
 
-complete -c cpu_info -l cores -d 'Number of cores'
-complete -c cpu_info -l threads -d 'Number of threads'
-complete -c cpu_info -l cpus -d 'Number of cpus'
+complete -c cpu_info -x -s p -l cores -d 'Number of cores'
+complete -c cpu_info -x -s l -l threads -d 'Number of threads'
+complete -c cpu_info -x -s c -l cpus -d 'Number of cpus'
+complete -c cpu_info -x -s h -l help -d 'Show usage'
+complete -c cpu_info -x -l all -d 'Show all informations'
